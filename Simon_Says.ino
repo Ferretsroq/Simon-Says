26005b2190e6ed78fc1f2facee7b62109d26e16c
Ferretsroq <Ferretsroq@gmail.com>
2016-01-06 13:45:29 -0500

    Incorporating Button Presses Into State Changes
    Right now this code does not work - the button presses are being read by
    two different functions, which makes neither of them work. If either of
    ChangeStateWithButtonPresses() or PrintButtonPresses() are called, they
    work. They do not work if both are called.

--- Simon_Says.ino
+++ Simon_Says.ino
@@ -6,14 +6,16 @@
 #include "State.h"
 
 // Global Variables
-const int g_blueLEDPin = 5;
-const int g_greenLEDPin = 4;
-const int g_redLEDPin = 2; 
+const int g_blueLEDPin = 2;
+const int g_greenLEDPin = 5;
+const int g_redLEDPin = 4; 
 const int g_yellowLEDPin = 3;
-const int g_blueButtonPin = 9;
-const int g_greenButtonPin = 10;
-const int g_redButtonPin = 8; 
+
+const int g_blueButtonPin = 8;
+const int g_greenButtonPin = 9;
+const int g_redButtonPin = 10; 
 const int g_yellowButtonPin = 11;
+
 long g_timerForLEDFlashes = 0;
 bool g_lightOnOrOff = 0;
 Sequence g_testSequence = Sequence(240);
@@ -28,14 +30,15 @@ bool g_yellowButtonState = 0;
 bool g_lastYellowButtonState = 0;
 
 std::vector<State> g_states;
-int currentState = 0;
+int g_currentState = 0;
 
 // Function Prototypes
 void ShowColor(Sequence::Color);
 void ShowWholeSequence(Sequence);
 void TurnColorOff(Sequence::Color colorToTurnOff);
 signed char DetectButtonStateChange(int);
-void PrintButtonPresses(int);
+void PrintButtonPresses(void);
+void ChangeStateWithButtonPresses(void);
 
 void setup() 
 {
@@ -62,16 +65,9 @@ void loop()
 {
   // put your main code here, to run repeatedly:
   ShowWholeSequence(g_testSequence);
-  currentState = g_states[currentState].nextState(2);
-  Serial.println(currentState);
-
-  DetectButtonStateChange(g_blueButtonPin);
-  DetectButtonStateChange(g_greenButtonPin);
-  DetectButtonStateChange(g_redButtonPin);
-  DetectButtonStateChange(g_yellowButtonPin);
-  
-  //PrintButtonPresses(g_redButtonPin);
-  
+  ChangeStateWithButtonPresses();
+  PrintButtonPresses();
+  //Serial.println(g_currentState);  
 }
 
 void ShowColor(Sequence::Color colorToShow)
@@ -144,8 +140,8 @@ signed char DetectButtonStateChange(int buttonOfInterest)
        g_blueButtonState = digitalRead(g_blueButtonPin);
        if(g_blueButtonState != g_lastBlueButtonState)
        {
-          if(g_blueButtonState) Serial.println("You pressed the blue button!");
-          else Serial.println("You released the blue button!");
+          if(g_blueButtonState) returnedButtonState = 1;
+          else returnedButtonState = 0;
        }
        g_lastBlueButtonState = g_blueButtonState;
        break;
@@ -154,8 +150,8 @@ signed char DetectButtonStateChange(int buttonOfInterest)
        g_greenButtonState = digitalRead(g_greenButtonPin);
        if(g_greenButtonState != g_lastGreenButtonState)
        {
-         if(g_greenButtonState) Serial.println("You pressed the green button!");
-         else Serial.println("You released the green button!");
+         if(g_greenButtonState) returnedButtonState = 1;
+         else returnedButtonState = 0;
        }
        g_lastGreenButtonState = g_greenButtonState;
        break;
@@ -164,8 +160,8 @@ signed char DetectButtonStateChange(int buttonOfInterest)
         g_redButtonState = digitalRead(g_redButtonPin);
         if(g_redButtonState != g_lastRedButtonState)
         {
-          if(g_redButtonState) Serial.println("You pressed the red button!");
-          else Serial.println("You released the red button!");
+          if(g_redButtonState) returnedButtonState = 1;
+          else returnedButtonState = 0;
         }
         g_lastRedButtonState = g_redButtonState;
         break;
@@ -174,8 +170,8 @@ signed char DetectButtonStateChange(int buttonOfInterest)
         g_yellowButtonState = digitalRead(g_yellowButtonPin);
         if(g_yellowButtonState != g_lastYellowButtonState)
         {
-          if(g_yellowButtonState) Serial.println("You pressed the yellow button!");
-          else Serial.println("You released the yellow button!");
+          if(g_yellowButtonState) returnedButtonState = 1;
+          else returnedButtonState = 0;
         }
         g_lastYellowButtonState = g_yellowButtonState;
         break;   
@@ -183,10 +179,50 @@ signed char DetectButtonStateChange(int buttonOfInterest)
   return returnedButtonState;
 }
 
-void PrintButtonPresses(int buttonPin)
+void PrintButtonPresses(void)
 {
-  signed char pressedButton = DetectButtonStateChange(buttonPin);
-  if(pressedButton == 1) Serial.println("You pressed a button!");
-  else if(pressedButton == 0) Serial.println("You released a button!");
+  int blueButtonState = DetectButtonStateChange(g_blueButtonPin);
+  int greenButtonState = DetectButtonStateChange(g_greenButtonPin);
+  int redButtonState = DetectButtonStateChange(g_redButtonPin);
+  int yellowButtonState = DetectButtonStateChange(g_yellowButtonPin);
+  
+  if(blueButtonState == 1) Serial.println("You pressed the blue button!");
+  else if(blueButtonState == 0) Serial.println("You released the blue button!");
+  
+  if(greenButtonState == 1) Serial.println("You pressed the green button!");
+  else if(greenButtonState == 0) Serial.println("You released the green button!");
+  
+  if(redButtonState == 1) Serial.println("You pressed the red button!");
+  else if(redButtonState == 0) Serial.println("You released the red button!");
+  
+  if(yellowButtonState == 1) Serial.println("You pressed the yellow button!");
+  else if(yellowButtonState == 0) Serial.println("You released the yellow button!");
+}
+
+void ChangeStateWithButtonPresses(void)
+{
+  if(DetectButtonStateChange(g_blueButtonPin) == 1)
+  {
+    g_currentState = g_states[g_currentState].nextState(0); 
+    Serial.println(g_currentState); 
+  }
+  
+  if(DetectButtonStateChange(g_greenButtonPin) == 1)
+  {
+    g_currentState = g_states[g_currentState].nextState(1); 
+    Serial.println(g_currentState);
+  }
+ 
+  if(DetectButtonStateChange(g_redButtonPin) == 1)
+  {
+    g_currentState = g_states[g_currentState].nextState(2); 
+    Serial.println(g_currentState);
+  }
+  
+  if(DetectButtonStateChange(g_yellowButtonPin) == 1)
+  {
+    g_currentState = g_states[g_currentState].nextState(3);
+    Serial.println(g_currentState);
+  }
 }
 
